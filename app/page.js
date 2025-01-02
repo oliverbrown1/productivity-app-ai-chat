a3eb3d550f59e4096d1621a5d4baf70c92ac3d0a
Oliver Brown <olivertouki@gmail.com>
2025-01-02 13:09:37 +0000

    AI assistant almost complete

--- app/page.js
+++ app/page.js
@@ -3,21 +3,29 @@
 import { useState } from "react";
 import Tasklist from "./tasklist.js"
 import Modal from "../components/Modal.js"
+import AI_chat from "../components/AI_chat.js"
 import PocketBase from 'pocketbase';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faPlus, faWandMagicSparkles } from '@fortawesome/free-solid-svg-icons'
 
 // pocket base DB api endpoint
 const pb = new PocketBase('http://127.0.0.1:8090');
 
 // home page with tasks
 export default function Page(){
-  const [modalVisibility, setModalVisibility] = useState(false);
+  const [taskModalVisibility, setTaskModalVisibility] = useState(false);
+  const [AIModalVisibility, setAIModalVisibility] = useState(false);
 
   // toggle modal visibility when task is added
-  function toggleModalVisibility(){
-    setModalVisibility(!modalVisibility);
+  function toggleTaskModalVisibility(){
+    setTaskModalVisibility(!taskModalVisibility);
   }
+
+  function toggleAIModalVisibility(){
+    setAIModalVisibility(!AIModalVisibility);
+  }
+
+
   // list of tasks
   const [tasks, setTasks] = useState([]);
   // fetch tasks from DB endpoint
@@ -43,11 +51,50 @@ export default function Page(){
     fetchData();
   }
 
+  function onClose(){
+    if(AIModalVisibility){
+      toggleAIModalVisibility
+    }
+    if(taskModalVisibility){
+      toggleTaskModalVisibility
+    }
+  }
+
   // delete task function passed onto tasklist
   async function deleteTask(id){
     const record = await pb.collection('Tasks').delete(id);
     fetchData();
   }
+
+    // calculate time in seconds given the fields to get time in seconds, and add data to DB
+  function handleForm(data){
+        const task_name = data.get("name");
+        const task_desc = data.get("desc");
+        
+        const hours = parseInt(data.get("hours")) || 0;
+        const minutes = parseInt(data.get("minutes")) || 0;
+        const seconds = parseInt(data.get("seconds")) || 0;
+        let time_in_seconds = (hours * 3600) + (minutes * 60) + seconds;
+
+        if(time_in_seconds <= 0){
+            // out of range
+            alert("Your task must take at least one second!!!")
+            return;
+        }
+        if(time_in_seconds > 36000){
+            alert("Your task must not exceed 10 hours, otherwise split it into different tasks!")
+            return;
+        }
+
+        const task = {
+            name: task_name,
+            desc: task_desc,
+            time: time_in_seconds
+        };
+        addTask(task);
+        onClose();
+  }
+
 
   // main page html
   return (
@@ -58,7 +105,10 @@ export default function Page(){
       </div>
       <div className="w-3/5 flex flex-col fixed top-1/2 bg-gray-200 justify-center items-center task-list-div">
         {/* add task button: + icon */}
-        <button className="block absolute top-0 right-0 m-2 text-slate-700 bg-gray-200"  onClick={toggleModalVisibility} type="button">
+        <button className="block absolute top-0 left-0 m-2 text-slate-700 bg-gray-200"  onClick={toggleAIModalVisibility} type="button">
+        <FontAwesomeIcon icon={faWandMagicSparkles} className="hover:text-slate-400"/>
+        </button>
+        <button className="block absolute top-0 right-0 m-2 text-slate-700 bg-gray-200"  onClick={toggleTaskModalVisibility} type="button">
           <FontAwesomeIcon icon={faPlus} size="lg" className="hover:text-slate-400"/> 
         </button>
         {/* tasklist div */}
@@ -66,7 +116,9 @@ export default function Page(){
           <Tasklist deleteTask = {deleteTask} fetchData = {fetchData} tasks={tasks}/>
         </div>
       </div>
-      <Modal visible={modalVisibility} onClose={toggleModalVisibility} addTask={addTask}/>
+      <Modal visible={taskModalVisibility} onClose={toggleTaskModalVisibility} addTask={handleForm}/>
+      <AI_chat visible={AIModalVisibility} onClose={toggleAIModalVisibility} addTask={handleForm}/>
+
     </div>
   );
 }
--- components/Modal.js
+++ components/Modal.js
@@ -6,34 +6,34 @@ export default function Modal({visible, onClose, addTask}){
         return null;
     }
 
-    // calculate time in seconds given the fields to get time in seconds, and add data to DB
-    function handleForm(data){
-        const task_name = data.get("name");
-        const task_desc = data.get("desc");
+    // // calculate time in seconds given the fields to get time in seconds, and add data to DB
+    // function handleForm(data){
+    //     const task_name = data.get("name");
+    //     const task_desc = data.get("desc");
         
-        const hours = parseInt(data.get("hours")) || 0;
-        const minutes = parseInt(data.get("minutes")) || 0;
-        const seconds = parseInt(data.get("seconds")) || 0;
-        let time_in_seconds = (hours * 3600) + (minutes * 60) + seconds;
+    //     const hours = parseInt(data.get("hours")) || 0;
+    //     const minutes = parseInt(data.get("minutes")) || 0;
+    //     const seconds = parseInt(data.get("seconds")) || 0;
+    //     let time_in_seconds = (hours * 3600) + (minutes * 60) + seconds;
 
-        if(time_in_seconds <= 0){
-            // out of range
-            alert("Your task must take at least one second!!!")
-            return;
-        }
-        if(time_in_seconds > 36000){
-            alert("Your task must not exceed 10 hours, otherwise split it into different tasks!")
-            return;
-        }
+    //     if(time_in_seconds <= 0){
+    //         // out of range
+    //         alert("Your task must take at least one second!!!")
+    //         return;
+    //     }
+    //     if(time_in_seconds > 36000){
+    //         alert("Your task must not exceed 10 hours, otherwise split it into different tasks!")
+    //         return;
+    //     }
 
-        const task = {
-            name: task_name,
-            desc: task_desc,
-            time: time_in_seconds
-        };
-        addTask(task);
-        onClose();
-    }
+    //     const task = {
+    //         name: task_name,
+    //         desc: task_desc,
+    //         time: time_in_seconds
+    //     };
+    //     addTask(task);
+    //     onClose();
+    // }
     
     // form to create new task in modal
     return (
@@ -41,7 +41,7 @@ export default function Modal({visible, onClose, addTask}){
             <div className="w-[600px] flex flex-col">
                 <div className="relative mt-5 top-1/2 bg-gray-200 p-10 shadow-xl">
                     <h2 className="text-center text-xl mb-2">Create new task</h2>
-                    <form action={handleForm} className="flex flex-col gap-3"> 
+                    <form action={addTask} className="flex flex-col gap-3"> 
                         <div className="flex flex-col">
                         <label className="text-slate-800">Title</label>
                         <input className="hover:bg-slate-200 p-2 m-3 border-slate-200 bg-slate-100 rounded focus:shadow-outline" type="text" name="name" placeholder="What do you need to do?" required/>
